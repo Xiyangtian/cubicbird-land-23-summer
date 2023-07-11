9a07a42a6165f6a132bd868d737fd2c53a1ae4ae
Xiyangtian <87839982+Xiyangtian@users.noreply.github.com>
2023-07-11 18:57:03 +0800

    treeElder

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -81,6 +81,16 @@ case "hxy级别3":return tiles.createTilemap(hex`1000100000000000000000000000000
 . 2 2 2 2 2 . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `, [myTiles.transparency16,sprites.dungeon.collectibleInsignia,sprites.dungeon.floorLight2,sprites.builtin.brick], TileScale.Sixteen);
+case "wn古树内部":return tiles.createTilemap(hex`0a00080001010101030505050506010101010402020202070101010104020202020703050505020202020207040202020202020202070a0808080202080808090b0c0b0c04070c0b0c0b0d0b0c0c04070b0c0b0c`, img`
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.castle.tilePath5,sprites.castle.tilePath1,sprites.castle.tilePath4,sprites.castle.tilePath2,sprites.castle.tilePath3,sprites.castle.tilePath6,sprites.castle.tilePath8,sprites.castle.tilePath9,sprites.castle.tilePath7,sprites.castle.tileGrass1,sprites.castle.tileGrass3,sprites.builtin.forestTiles0], TileScale.Sixteen);
 }
         return null;
     })
