668a3d3c3f45dbfa91c4e98e41c3847d561173d6
Xiyangtian <87839982+Xiyangtian@users.noreply.github.com>
2023-07-16 21:09:44 +0800

    第一期集成

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -36,24 +36,24 @@ namespace myTiles {
 . . . . . . . . . . 
 `, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.castle.tilePath5,sprites.castle.tilePath1,sprites.castle.tilePath4,sprites.castle.tilePath2,sprites.castle.tilePath3,sprites.castle.tilePath6,sprites.castle.tilePath8,sprites.castle.tilePath9,sprites.castle.tilePath7,sprites.castle.tileGrass1,sprites.castle.tileGrass3,sprites.builtin.forestTiles0], TileScale.Sixteen);
             case "hxy级别2":return tiles.createTilemap(hex`110010000202020202020202010202020202020202020202020202020202020202020202020202020202020202020202020202020202020202070707020207070702020707070202020207070702020707070202070707020202020707070202070707020207070702020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020303030303030202020303030303030303020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020504060202020202020202`, img`
-. . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . 
-. . 2 2 2 . . 2 2 2 . . 2 2 2 . . 
-. . 2 2 2 . . 2 2 2 . . 2 2 2 . . 
-. . 2 2 2 . . 2 2 2 . . 2 2 2 . . 
-. . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . 
-2 2 2 2 2 2 . . . 2 2 2 2 2 2 2 2 
-. . . . . 2 . . . 2 . . . . . . . 
-. . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . 
-. . . . . 2 . . . 2 . . . . . . . 
-`, [myTiles.transparency16,sprites.dungeon.collectibleInsignia,sprites.dungeon.floorLight2,sprites.builtin.brick,tiles.util.arrow7,tiles.util.arrow15,tiles.util.arrow5,sprites.dungeon.darkGroundNorthWest1], TileScale.Sixteen);
-        }
+            . . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . . 
+            . . 2 2 2 . . 2 2 2 . . 2 2 2 . . 
+            . . 2 2 2 . . 2 2 2 . . 2 2 2 . . 
+            . . 2 2 2 . . 2 2 2 . . 2 2 2 . . 
+            . . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . . 
+            2 2 2 2 2 2 . . . 2 2 2 2 2 2 2 2 
+            . . . . . 2 . . . 2 . . . . . . . 
+            . . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . . 
+            . . . . . 2 . . . 2 . . . . . . . 
+            `, [myTiles.transparency16,sprites.dungeon.collectibleInsignia,sprites.dungeon.floorLight2,sprites.builtin.brick,tiles.util.arrow7,tiles.util.arrow15,tiles.util.arrow5,sprites.dungeon.darkGroundNorthWest1], TileScale.Sixteen);
+       }
         return null;
     })
 
